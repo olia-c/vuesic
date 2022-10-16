8ba0fc9a5484c7aa23306baab88bf7790d7f6249
olac <olia.chezhina@gmail.com>
2022-10-17 00:10:01 +0300

    install vee-validate/rules

--- src/plugins/validation.js
+++ src/plugins/validation.js
@@ -1,7 +1,9 @@
-import { Form as VeeFrom, Field as VeeField } from "vee-validate";
+import { Form as VeeFrom, Field as VeeField, defineRule } from "vee-validate";
+import { required } from "@vee-validate/rules";
 export default {
   install(app) {
     app.component("VeeForm", VeeFrom);
     app.component("VeeField", VeeField);
+    defineRule("required", required);
   },
 };
